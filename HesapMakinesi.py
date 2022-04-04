8832325e0dd4db59abe022fa60a17d8ce5c99c31
ourkan95@hotmail.com <ourkan95@hotmail.com>
2022-04-04 19:19:52 +0300

    Bölme işlemi eklendi(v2.0)

--- HesapMakinesi.py
+++ HesapMakinesi.py
@@ -6,10 +6,14 @@ print("Hesap Makinesine Hoş Geldiniz... Yapmak istediğiniz İşlemi Seçiniz:"
 islem = int(input("Toplama için 1 Bölme için 2: "))
 
 if islem == 1:
+
     x = float(input("Toplanmak istenen ilk sayıyı giriniz: "))
     y = float(input("Toplanmak istenen ikinci sayıyı giriniz: "))
     
     sonuc = x + y
-    print(sonuc)
+    print("İşlemin sonucu: " + str(sonuc))
 if islem == 2:
-    print("Bölme özelliği henüz eklenmedi")    
+       x = float(input("Payı giriniz: "))
+       y = float(input("Paydayı giriniz: "))
+       sonuc = x / y
+       print("İşlemin sonucu: " + str(sonuc))
